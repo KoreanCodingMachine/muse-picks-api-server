9acb99ff3074495b6724b3f85267599eb5a45e44
김주형 <59787529+KoreanCodingMachine@users.noreply.github.com>
2024-09-14 11:50:01 +0900

    init project

---
+++ src/main/java/com/kjh/musepicks/MusepicksApplication.java
@@ -0,0 +1,13 @@
+package com.kjh.musepicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusepicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusepicksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjh/musepicks/MusepicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh.musepicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusepicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
