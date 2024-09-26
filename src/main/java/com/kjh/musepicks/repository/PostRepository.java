335dffa8deb0c18666acb46e0c0ce9d5065fc978
김주형 <59787529+KoreanCodingMachine@users.noreply.github.com>
2024-09-26 11:46:09 +0900

    PostRepository jpql수정

--- src/main/java/com/kjh/musepicks/repository/PostRepository.java
+++ src/main/java/com/kjh/musepicks/repository/PostRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT p FROM Post p JOIN FETCH p.postCategories")
+//    @Query("SELECT p FROM Post p JOIN FETCH p.postCategories")
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.postCategories")
     List<Post> findAllPostsWithCategory();
+
 }
